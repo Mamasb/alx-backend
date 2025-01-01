1f1bff20ea37b392ab4f739f67e51045974f83ee
mamasb <bbitnrb075812@spu.ac.ke>
2025-01-01 18:56:08 +0300

    feat: add Redis pub/sub system with publisher and subscriber scripts

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -2,22 +2,17 @@ import redis from 'redis';
 
 const publisher = redis.createClient();
 
-publisher.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
-
-publisher.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
-});
+publisher.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
+publisher.on('connect', () => console.log('Redis client connected to the server'));
 
 function publishMessage(message, time) {
-  setTimeout(() => {
-    console.log(`About to send ${message}`);
-    publisher.publish('ALXchannel', message);
-  }, time);
+    setTimeout(() => {
+        console.log(`About to send ${message}`);
+        publisher.publish('holberton school channel', message);
+    }, time);
 }
 
-publishMessage('ALX Student #1 starts course', 100);
-publishMessage('ALX Student #2 starts course', 200);
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
 publishMessage('KILL_SERVER', 300);
-publishMessage('ALX Student #3 starts course', 400);
+publishMessage('Holberton Student #3 starts course', 400);
